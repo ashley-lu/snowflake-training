723aa6687f71915b8f35eb031a494df733a5db50
AnonSameer <90491281+AnonSameer@users.noreply.github.com>
2022-07-29 15:12:43 -0700

    Added query

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ conn = sf.connect(
         account=secret.get('account'),
         user=secret.get('user'),
         password=secret.get('password'),
-        database='ECONOMY_DATA_ATLAS',
-        schema='ECONOMY',
-        warehouse='COMPUTE_WH')
+        database='REALESTATE_DATA_ATLAS',
+        schema='REALESTATE',
+        warehouse='COMPUTE_WH',
+        role="ACCOUNTADMIN"
+)
+
+query = '''SELECT * FROM "REALESTATE"."ZRHVI2020JUL" WHERE ("Region Name" = 'Los Angeles-Long Beach-Anaheim, CA' OR "Region Name" = 'San Diego, CA' OR "Region Name" = 'Chico, CA') AND "Indicator Name" = 'ZHVI - Single Family Residence' ORDER BY "Date"'''
+
+#print(query)
+inflation_df = pd.read_sql_query(query, conn)
+
+st.write(inflation_df)
+
+
 
 # test
